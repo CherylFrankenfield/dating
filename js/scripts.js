49e174b5aaea140387848f7c8f4b6439e5615384
cherylfrankenfield and Holly Wilkalis <cherylfrankenfield@>
2017-10-19 15:31:35 -0700

    Add branch logic. Add show/hide content

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,33 @@
 $(document).ready(function() {
+  $("form#form").change(function(event) {
+    var answerOne = parseInt(select("#beverage")).val();
+    var answerTwo = parseInt(select("#animal")).val();
+    var answerThree = parseInt(select("#boyfriend")).val();
+    var total = answerOne + answerTwo + answerThree;
+      return total;
 
+    if (total === 3) {
+      $("#celebrity1").show();
+    }
+    else if (total === 4) {
+      $("#celebrity2").show();
+    }
+    else if (total === 5) {
+      $("#celebrity3").show();
+    }
+    else  {
+      $("#celebrity4").show();
+    }
+
+    // if (outputAnimal === 'turtle')  {
+    //   $('#turtle').show();
+    //   $('#snake','#otter').hide();
+    // } else if (outputAnimal === 'snake') {
+    //   $('#snake').show();
+    // }
+    // else {
+    //   $('#otter').show();
+    //
+  });
 
 });
